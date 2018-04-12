a3ac20c33cdddd1d7eaf3c1caeeecaa63a54bc78
Ther Matthews <emeraldvision@gmail.com>
2018-04-12 15:37:23 -0500

    new post and displayed posts on single page

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:BlogBuild@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'K5_e55@-P^-cHXc9'
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    body = db.Column(db.String(1000000))
+    
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return redirect('/blog', code=307)
+
+@app.route('/blog', methods=['GET', 'POST'])
+def main_display():
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        new_blog = Blog(blog_title, blog_body)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blog_posts = Blog.query.all()
+    return render_template('blog.html', posts=blog_posts)
+
+
+if __name__ == '__main__':
+    app.run()
