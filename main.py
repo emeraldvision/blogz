69d6705a5a6479bcfb086d430fbe48944c9e24e6
Ther Matthews <emeraldvision@gmail.com>
2018-04-21 09:39:37 -0500

    displays single post after submitting or clicking title

--- main.py
+++ main.py
@@ -34,12 +34,20 @@ def main_display():
             new_blog = Blog(blog_title, blog_body)
             db.session.add(new_blog)
             db.session.commit()
+            return redirect('/blog?id=' + str(new_blog.id))
         else:
             if not blog_title or not blog_title.strip():
                 flash("The post needs a title")
             if not blog_body or not blog_title.strip():
                 flash("The post body can't be empty")
             return render_template('newpost.html', page_title="Add a Blog", blog_title=blog_title, blog_body=blog_body)
+
+    blog_id = request.args.get('id')
+    if blog_id:
+        current_blog = Blog.query.filter_by(id=blog_id).first()
+        if current_blog:
+            return render_template('singlepost.html', page_title=current_blog.title, blog_body=current_blog.body)
+        flash("That blog post cannot be found")
 
     blog_posts = Blog.query.all()
     return render_template('blog.html', page_title="Build a Blog", posts=blog_posts)
